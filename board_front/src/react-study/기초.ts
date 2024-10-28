60f54bd2eee6b90ceea41af6fb65763224fb679f
earthmin <fltk4286@naver.com>
2024-10-28 11:49:25 +0900

    fontend project

---
+++ board_front/src/react-study/기초.ts
@@ -0,0 +1,3 @@
+// 단축키.ts 
+
+export const tmp= "";
---
+++ board_front/src/프로젝트환경설정/index.ts
@@ -0,0 +1,25 @@
+export const tmp = '';
+
+/*
+
+  ! 프로젝트 환경 설정 
+
+  1. 외부 라이브러리 설치 (의존성)
+  : react-router-dom - 프로젝트 간의 페이징 처리 
+  : react-cookie - 프로젝트 서버 관리 
+  : axios - 비동기 처리
+  : zustand - 전역 상태 관리
+
+  - npm i 뒤에 각 라이브러리명 사용 
+  
+  2. 공통 상수, 공통 함수, 공통 타입 명시 
+  : 화면 URL 
+  : 네비게이션 구조 작성 
+
+  3. root 경로이 index.tsx에서 BrowserRouter 등록 
+    +) App.tsx 에 Routes, Route를 사용하여 네비게이션 지정 
+  
+
+
+
+*/
